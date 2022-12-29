f9158ca33d2efedc70d65902b9ebd7dea0c1905b
Dan DeMarco <email@email>
2022-12-29 13:18:33 -0600

    timetravel

---
+++ questions.js
@@ -0,0 +1,44 @@
+let questions = [
+    {
+        'question': 'Sometimes an unsanctioned time loop is okay.',
+        'preference': 'disagree',
+        'confirmation': `<strong>Correct.</strong><br>All time loops require clearance from the TRA.<br>Recreational time 
+            travel is illegal.`,
+        'correction': `<strong>Incorrect.</strong><br>All time loops require clearance from the TRA.<br>Recreational time 
+            travel is illegal.`,
+    },
+    {
+        'question': 'It\'s okay to tell your parents you\'re in a time loop.',
+        'preference': 'disagree',
+        'confirmation': `<strong>Correct.</strong><br>For the sake of timeline consistency, all time loops are 
+            confidential.<br>Telling anyone you are currently looping is a highly chaotic event, and illegal.`,
+        'correction': `<strong>Incorrect.</strong><br>For the sake of timeline consistency, all time loops are 
+            confidential.<br>Telling anyone you are currently looping is a highly chaotic event, and illegal.`,
+    },
+    {
+        'question': 'Timeline disruption is unavoidable and immediate.',
+        'preference': 'agree',
+        'confirmation': `<strong>Correct.</strong><br>Any time loop, no matter how short they seem to you, can create 
+            massive anomalies in time that are impossible to predict.<br>That's  why the TRA has made it our mission to 
+            make time travel as safe and controlled as possible.`,
+        'correction': `<strong>Incorrect.</strong><br>Any time loop, no matter how short they seem to you, can create 
+            massive anomalies in time that are impossible to predict.<br>That's  why the TRA has made it our mission to 
+            make time travel as safe and controlled as possible.`,
+    },
+    {
+        'question': 'You should report any anomalies in your timeline as soon as you notice them.',
+        'preference': 'agree',
+        'confirmation': `<strong>Correct.</strong><br>During your time loop, ANY anomalies you notice, no matter how 
+             trivial, must be reported ANONYMOUSLY to the TRA hotline as soon as they are noticed.`,
+        'correction': `<strong>Incorrect.</strong><br>During your time loop, ANY anomalies you notice, no matter how 
+             trivial, must be reported ANONYMOUSLY to the TRA hotline as soon as they are noticed.`,
+    },
+    {
+        'question': 'Time travel into the future is possible.',
+        'preference': 'disagree',
+        'confirmation': `<strong>Correct.</strong><br>People with the ability to travel through time are only able to 
+             travel backwards within their own timeline.<br>As far as we know, time travel into the future is impossible.`,
+        'correction': `<strong>Incorrect.</strong><br>People with the ability to travel through time are only able to 
+             travel backwards within their own timeline.<br>As far as we know, time travel into the future is impossible.`,
+    },
+]
