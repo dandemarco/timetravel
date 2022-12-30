70da20f9acbdb33ae7428dd622297cffab3f9d84
Dan DeMarco <email@email>
2022-12-30 00:00:02 -0600

    reduced "correction" and "confirmation" into single "message" to reduce redundant code

--- script.js
+++ script.js
@@ -24,11 +24,15 @@ correctionSection.style.visibility = "collapse"
 //Hide review button
 reviewButton.style.visibility = "collapse"
 
+newStatement()
+
 okButton.addEventListener("click", function() {
     correctionSection.style.visibility="collapse"
     iconZone.innerHTML = ''
     agreeButton.style.visibility="visible"
     disagreeButton.style.visibility="visible"
+    console.log('Correct responses: ' + correctResponses)
+    console.log('Incorrect responses: ' + incorrectResponses)
     newStatement()
 })
 
@@ -36,4 +40,79 @@ reviewButton.addEventListener("click", function() {
     reviewQuiz()
 })
 
-newStatement()
+
+//newStatement() displays a new quiz statement on the screen for the user to respond to
+function newStatement() {
+    let [statement, preference, message] = lookup()
+
+    console.log(history)
+    statementHeader.innerHTML = statement
+
+    agreeButton.addEventListener("click", function() {
+        let userResponse = "agree"
+        checkResponse(statement, userResponse, preference, message)
+    })
+
+    disagreeButton.addEventListener("click", function() {
+        let userResponse = "disagree"
+        checkResponse(statement, userResponse, preference, message)
+    })
+}
+
+
+//lookup() pulls a random statement entry from the statements.js dictionary
+function lookup() {
+    let lookupLoop = true
+
+    while (lookupLoop) {
+        let randomEntry = statements[Math.floor(Math.random() * statements.length)]
+
+        let statement = randomEntry.statement
+        let preference = randomEntry.preference
+        let message = randomEntry.message
+
+        if (history.length === statements.length) {
+            console.log('Boop')
+            let statement = "Quiz complete."
+
+            reviewButton.style.visibility="visible"
+            agreeButton.style.visibility="collapse"
+            disagreeButton.style.visibility="collapse"
+
+            return [statement, preference, message]
+
+        } else if (!history.includes(statement)) {
+            history.push(statement)
+
+            return [statement, preference, message]
+        }
+    }
+}
+
+//checkResponse() checks the user response against the preferred response, and displays
+//either a confirmation or a correction
+function checkResponse(statement, userResponse, preference, message) {
+    correctionSection.style.visibility="visible"
+    agreeButton.style.visibility="collapse"
+    disagreeButton.style.visibility="collapse"
+    if (userResponse === preference) {
+        correctResponses.push(statement)
+        console.log('Correct responses: ' + correctResponses)
+        iconZone.innerHTML = `<img src="correct.gif" alt="Correct icon">`
+        correctionText.innerHTML = `<strong>Correct.</strong><br>${message}`
+    } else {
+        incorrectResponses.push(statement)
+        console.log('Incorrect responses: ' + incorrectResponses)
+        iconZone.innerHTML = `<img src="incorrect.png" alt="Incorrect icon">`
+        correctionText.innerHTML = `<strong>Incorrect.</strong><br>${message}`
+    }
+}
+
+//reviewQuiz() reviews what the user got right and wrong
+function reviewQuiz() {
+    console.log('Correct responses: ' + correctResponses)
+    console.log('Incorrect responses: ' + incorrectResponses)
+}
+
+
+
--- statements.js
+++ statements.js
@@ -2,43 +2,39 @@ let statements = [
     {
         'statement': 'Sometimes an unsanctioned time loop is okay.',
         'preference': 'disagree',
-        'confirmation': `<strong>Correct.</strong><br>All time loops require clearance from the TRA.<br>Recreational time 
-            travel is illegal.`,
-        'correction': `<strong>Incorrect.</strong><br>All time loops require clearance from the TRA.<br>Recreational time 
-            travel is illegal.`,
+        'message': `All time loops require clearance from the TRA.<br>
+            <strong>Recreational time travel is illegal.</strong>`,
     },
     {
         'statement': 'It\'s okay to tell your parents you\'re in a time loop.',
         'preference': 'disagree',
-        'confirmation': `<strong>Correct.</strong><br>For the sake of timeline consistency, all time loops are 
-            confidential.<br>Telling anyone you are currently looping is a highly chaotic event, and illegal.`,
-        'correction': `<strong>Incorrect.</strong><br>For the sake of timeline consistency, all time loops are 
+        'message': `For the sake of timeline consistency, all time loops are 
             confidential.<br>Telling anyone you are currently looping is a highly chaotic event, and illegal.`,
     },
     {
         'statement': 'Timeline disruption is unavoidable and immediate.',
         'preference': 'agree',
-        'confirmation': `<strong>Correct.</strong><br>Any time loop, no matter how short they seem to you, can create 
-            massive anomalies in time that are impossible to predict.<br>That's  why the TRA has made it our mission to 
-            make time travel as safe and controlled as possible.`,
-        'correction': `<strong>Incorrect.</strong><br>Any time loop, no matter how short they seem to you, can create 
+        'message': `Any time loop, no matter how short they seem to you, can create 
             massive anomalies in time that are impossible to predict.<br>That's  why the TRA has made it our mission to 
             make time travel as safe and controlled as possible.`,
     },
     {
         'statement': 'You should report any anomalies in your timeline as soon as you notice them.',
         'preference': 'agree',
-        'confirmation': `<strong>Correct.</strong><br>During your time loop, ANY anomalies you notice, no matter how 
-             trivial, must be reported ANONYMOUSLY to the TRA hotline as soon as they are noticed.`,
-        'correction': `<strong>Incorrect.</strong><br>During your time loop, ANY anomalies you notice, no matter how 
-             trivial, must be reported ANONYMOUSLY to the TRA hotline as soon as they are noticed.`,
+        'message': `During your time loop, ANY anomalies, no matter how trivial, must be reported 
+            <strong>ANONYMOUSLY</strong> to the TRA hotline as soon as they are noticed.`,
     },
     {
         'statement': 'Time travel into the future is possible.',
         'preference': 'disagree',
-        'confirmation': `<strong>Correct.</strong><br>People with the ability to travel through time are only able to 
+        'message': `People with the ability to travel through time are only able to 
              travel backwards within their own timeline.<br>As far as we know, time travel into the future is impossible.`,
-        'correction': `<strong>Incorrect.</strong><br>People with the ability to travel through time are only able to 
-             travel backwards within their own timeline.<br>As far as we know, time travel into the future is impossible.`,
+    },
+    {
+        'statement': 'Any deviation from your time plan could result in jail time.',
+        'preference': 'agree',
+        'message': `Your time plan is a rigorous itinerary of events that you must 
+              follow exactly, for the sake of timeline consistency.<br><strong>Deviating from your time plan is grounds 
+              for a criminal investigation.</strong>`,
     },
 ]
